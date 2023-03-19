f0e9fc6ed3f09966cf74eae28ace9b1fd488b788
a-mhamdi <a_mhamdi@outlook.com>
2023-03-19 21:12:44 +0100

    bat file to automate labs

---
+++ TP/ctrl.bat
@@ -0,0 +1,13 @@
+::OPEN PDF FILE
+start "" c:\ctrl\lab-model-ctrl.pdf
+::CHANGE DIRECTORY
+cd c:\ctrl
+::ACTIVATE VENV
+env\Scripts\activate.bat && env\Scripts\jupyter-notebook.exe
+
+::CHANGE DIRECTORY & START MATLAB
+::cd C:\Users\admin\Desktop
+::matlab
+
+exit
+
